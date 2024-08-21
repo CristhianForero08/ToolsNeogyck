4939d57d0ef02e0ad663e84671feb7562e35eca9
Your Name <cristhianforerobello@gmail.com>
2024-08-20 21:42:51 -0500

    Configured environment variables 12

--- NeogyckSeo/settings.py
+++ NeogyckSeo/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
